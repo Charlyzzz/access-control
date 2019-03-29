e13c48a9436c9719966b8f8f74b813047c995005
Charlyzzz <erwincdl@gmail.com>
2019-03-29 15:49:52 -0300

    Adds pin interaction

--- cardReader.js
+++ cardReader.js
@@ -1,10 +1,13 @@
 const PN532 = require('pn532').PN532;
 const SerialPort = require('serialport');
+const gpio = require('rpi-gpio')
+const gpiop = gpio.promise;
+const PIN = 37;
 
 function cardReaderViaSerialPort(port = '/dev/ttyAMA0', cfg = { baudRate: 115200, pollInterval: 2000 }, portType = SerialPort) {
   const serialPort = new SerialPort(port, cfg);
   const nfcReader = new PN532(serialPort);
-  return new CardReader(nfcReader, () => Date.now, 3000);
+  return new CardReader(nfcReader, Date.now, 3000);
 }
 
 function CardReader(nfcReader, timer, pollingInterval) {
@@ -15,6 +18,10 @@ function CardReader(nfcReader, timer, pollingInterval) {
 
 CardReader.prototype.onTag = function onTag(callback) {
   this._reader.on('ready', () => {
+    gpiop.setup(PIN, gpio.DIR_OUT)
+      .then(() => {
+        return gpiop.write(PIN, true)
+      });
     let lastTagDetected = 0;
     let lastUID = null;
     this._reader.on('tag', ({ uid }) => {
