4a552ada59a213332ff11212dcb43f8150e39f5d
Charlyzzz <erwincdl@gmail.com>
2019-03-29 15:19:39 -0300

    Improves debouncing

--- cardReader.js
+++ cardReader.js
@@ -1,7 +1,7 @@
 const PN532 = require('pn532').PN532;
 const SerialPort = require('serialport');
 
-function cardReaderViaSerialPort(port = '/dev/ttyAMA0', cfg = { baudRate: 115200, pollInterval: 3000 }, portType = SerialPort) {
+function cardReaderViaSerialPort(port = '/dev/ttyAMA0', cfg = { baudRate: 115200, pollInterval: 1000 }, portType = SerialPort) {
   const serialPort = new SerialPort(port, cfg);
   const nfcReader = new PN532(serialPort);
   return new CardReader(nfcReader, Date);
@@ -15,18 +15,20 @@ function CardReader(nfcReader, timer) {
 
 CardReader.prototype.onTag = function onTag(callback) {
   this._reader.on('ready', () => {
-    const lastEvent = null;
+    let lastEvent = 0;
     this._reader.on('tag', (tag) => {
       const now = this.timer.now();
-      console.log("last", lastEvent);
-      console.log("now", now);
+      const ellapsedTime = now - lastEvent;
+      console.log("ellapsed", ellapsedTime);
       if (lastEvent == null || now - lastEvent > this.pollingInterval) {
         lastEvent = now;
         callback(tag);
       }
+      lastEvent = now;
     });
   });
 }
 
 exports.CardReader = CardReader
 exports.cardReaderViaSerialPort = cardReaderViaSerialPort
+
