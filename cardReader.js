0c18e074bd60f9b25097a0c5143cc27cd45c699f
Charlyzzz <erwincdl@gmail.com>
2019-03-29 15:21:54 -0300

    Fixes variable typo

--- cardReader.js
+++ cardReader.js
@@ -4,24 +4,24 @@ const SerialPort = require('serialport');
 function cardReaderViaSerialPort(port = '/dev/ttyAMA0', cfg = { baudRate: 115200, pollInterval: 2000 }, portType = SerialPort) {
   const serialPort = new SerialPort(port, cfg);
   const nfcReader = new PN532(serialPort);
-  return new CardReader(nfcReader, Date);
+  return new CardReader(nfcReader, () => Date.now, 3000);
 }
 
-function CardReader(nfcReader, timer) {
+function CardReader(nfcReader, timer, pollingInterval) {
   this._reader = nfcReader;
-  this.timer = timer;
-  this.pollingInterval = 3000;
+  this.now = timer;
+  this.pollingInterval = pollingInterval;
 }
 
 CardReader.prototype.onTag = function onTag(callback) {
   this._reader.on('ready', () => {
     let lastTagDetected = 0;
     this._reader.on('tag', (tag) => {
-      const now = this.timer.now();
+      const now = this.now();
       const ellapsedTime = now - lastTagDetected;
       console.log("ellapsed", ellapsedTime);
       if (now - lastTagDetected > this.pollingInterval) {
-        lastEvent = now;
+        lastTagDetected = now;
         callback(tag);
       }
     });
