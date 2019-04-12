f62dc88fb2a0453addd3d1e912aafd392b1be419
Erwin Debusschere <erwincdl@gmail.com>
2019-04-12 17:49:45 -0300

    Rollbacks reader instantiation

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { cardReaderViaSerialPort } = require('./src/cardReader');
 const reportNewTagDetected = require('./src/reporter');
-cardReaderViaSerialPort().then((nfcReader) => {
-  nfcReader.onTag((uid) => {
-    console.log('UUID: ', uid);
-    reportNewTagDetected({ uid, timestamp: Date.now() })
-      .catch(console.error);
-  });
-})
+const nfcReader = cardReaderViaSerialPort();
+
+nfcReader.onTag((uid) => {
+  console.log('UUID: ', uid);
+  reportNewTagDetected({ uid, timestamp: Date.now() })
+    .catch(console.error);
+});
