503ba438772d2d1af41ec451d240e91d206b2ae2
Erwin Debusschere <erwincdl@gmail.com>
2019-09-26 14:34:26 -0300

    Adds winston timestamp

--- src/logger.js
+++ src/logger.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 
 const logger = winston.createLogger({
   transports: [
-    new winston.transports.Console()
+    new winston.transports.Console()({ 'timestamp': true })
   ]
 });
 
